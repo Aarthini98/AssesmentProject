b607040701bfc3dd75def1125c61e9014e871ba3
Aarthini98 <60978766+Aarthini98@users.noreply.github.com>
2020-02-17 12:34:43 +0530

    delete function

--- apifetchandfav/src/favoriteList.js
+++ apifetchandfav/src/favoriteList.js
@@ -8,6 +8,22 @@ export default class FavoriteList extends React.Component {
 
     this.state = {};
   }
+  
+  removeFavoritee = favorite => {
+    const { favorites } = this.props;
+      this.setState({ favorites: this.favorites.filter(favorite => favorites.userid !== favorite)});
+    
+  };
+  removeFavorite(e) {
+    // var FavoriteList = this.props; // make a separate copy of the array
+    const { favorites } = this.props;
+    var index = e;
+    
+      favorites.splice(index, 1);
+      // this.setState({favorites:FavoriteList});
+      window.location.reload();
+  }
+
 
   render() {
     const { favorites } = this.props;
@@ -26,7 +42,7 @@ export default class FavoriteList extends React.Component {
                       <p>TITLE: {user.title}</p>
                       <p>BODY: {user.body}</p>
                     </div>
-                    <button >
+                    <button onClick={(event) => this.removeFavorite(event, user)}>
                       Delete
                     </button>
                   </li>
